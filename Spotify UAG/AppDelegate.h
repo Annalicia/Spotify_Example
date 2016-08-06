cd31c7eb28efb9248e9036b144e73b5e9ac7ba3d
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2016-08-06 07:36:51 -0700

    Initial Commit

---
+++ Spotify UAG/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Spotify UAG
+//
+//  Created by Walter Gonzalez Domenzain on 06/08/16.
+//  Copyright © 2016 UAG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Spotify UAG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Spotify UAG
+//
+//  Created by Walter Gonzalez Domenzain on 06/08/16.
+//  Copyright © 2016 UAG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
